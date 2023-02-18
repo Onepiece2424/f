20133352eada11a5bbf9b6c2d338505c5ae7c44b
rioto.nagano <kingdom950927@gmail.com>
2023-02-18 17:47:50 +0900

    Active JobのバックエンドにSidekiqを使用することを宣言

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,17 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module AsyncMailerApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Active JobのバックエンドにSidekiqを使用することを宣言
+    config.active_job.queue_adapter = :sidekiq
+
+    # タイムゾーンの設定
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
   end
 end
