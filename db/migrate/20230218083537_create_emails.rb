0b9e5d823f1b7271b6e716115971528cff3d246f
rioto.nagano <kingdom950927@gmail.com>
2023-02-18 17:40:10 +0900

    マイグレーションの実行

--- db/migrate/20230218083537_create_emails.rb
+++ db/migrate/20230218083537_create_emails.rb
@@ -1,11 +1,11 @@
 class CreateEmails < ActiveRecord::Migration[6.1]
   def change
     create_table :emails do |t|
-      t.string :to
-      t.string :from
-      t.string :subject
-      t.text :body
-      t.datetime :sent_at
+      t.string :to, null: false
+      t.string :from, null: false
+      t.string :subject, null: false
+      t.text :body, null: false
+      t.datetime :sent_at, null: false
 
       t.timestamps
     end
