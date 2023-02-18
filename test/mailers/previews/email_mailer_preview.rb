ab4c7b7cadfb5f560605ba94a5d5c4b121d5517d
rioto.nagano <kingdom950927@gmail.com>
2023-02-19 07:44:27 +0900

    プレビュー機能の作成

--- test/mailers/previews/email_mailer_preview.rb
+++ test/mailers/previews/email_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/email_mailer
 class EmailMailerPreview < ActionMailer::Preview
+  def send_mail
+    email = Email.new(to: "test@gamil.com", from: "gyoraigirl@gmail.com", subject: "天の助へ", body: "私はどんな小さなおふざけも許さない…なぜなら私は魚雷だから", sent_at: Time.local(2023, 2, 19))
 
+    EmailMailer.send_mail(email).deliver_now
+  end
 end
