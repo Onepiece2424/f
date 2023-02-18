9a002e8faef281547c1bc2a39ca8ebf2fc0dce55
rioto.nagano <kingdom950927@gmail.com>
2023-02-19 07:04:46 +0900

    メールに画像を送付する機能を追加

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,6 +1,9 @@
 class EmailMailer < ApplicationMailer
   def send_mail(email)
     @email = email
+
+    attachments['20220430_膝丸燈の画像.jpeg'] = File.read('tmp/20220430_膝丸燈の画像.jpeg')
+
     mail(
       from: @email.from,
       to:   @email.to,
