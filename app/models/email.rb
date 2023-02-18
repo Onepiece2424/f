8adb1a3165aea97cf44565357e1c556ca5992e96
rioto.nagano <kingdom950927@gmail.com>
2023-02-18 17:42:31 +0900

    バリデーションの作成

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,7 @@
 class Email < ApplicationRecord
+  validates :to, presence: true
+  validates :from, presence: true
+  validates :subject, presence: true
+  validates :body, presence: true
+  validates :send_at, presence: true
 end
